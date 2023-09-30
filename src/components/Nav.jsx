19bdfc10032ef00a5a564ca585248a4bc1721ee8
Carol B <carolb@bu.edu>
2023-09-30 16:37:31 -0400

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,37 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+	return (
+		<header className="padding-x py-8 absolute z-10 w-full">
+			<nav className="flex justify-between items-center max-container">
+				<a href="/">
+					<img src={headerLogo} alt="Logo" width={130} height={29} />
+				</a>
+
+				<ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+					{" "}
+					{/* max-lg applies a maximum media query, meaning the ul will be hidden until 1024px */}
+					{navLinks.map((item) => (
+						<li key={item.label}>
+							<a
+								href={`${item.href}`}
+								className="font-montserrat leading-normal text-lg text-slate-gray"
+							>
+								{item.label}
+							</a>
+						</li>
+					))}
+				</ul>
+
+				{/* hamburger appears at screen sizes up to 1024px and then gets hidden */}
+				<div className="hidden max-lg:block">
+					<img src={hamburger} alt="hamburger" width={25} height={25} />
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Nav;
